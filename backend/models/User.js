533a5d863f0d5c89092d0a9516f8aa8dcb28876d
srivathsava06 <manupatisrivathsava@gmail.com>
2026-04-07 22:16:49 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -53,12 +53,8 @@ const userSchema = new mongoose.Schema(
 userSchema.pre('save', async function () {
   if (!this.isModified('password')) return;
   
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-  } catch (error) {
-    throw error;
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Compare password method
